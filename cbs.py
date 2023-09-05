6b410d5feb3765e8ecb26fb6132a90366cfe8344
tmoore1210 <93944509+tmoore1210@users.noreply.github.com>
2023-09-05 03:29:06 +0000

    Welcoming Code

--- cbs.py
+++ cbs.py
@@ -7,3 +7,5 @@ print("Well" + " " + user_name + " " + "nice to meet you! What college do you at
 college_name = str(input())
 
 print("Ahhh I see!" + " " + college_name + " " + "is a great college!")
+
+print("My goal is to help understand whether you are managing your money correctly.")
