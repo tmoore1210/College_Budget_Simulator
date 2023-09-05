f407d92acb2acbb5ad27d72b5e9079de496cd38f
TrentonMoore <8728754-TrentonMoore@users.noreply.replit.com>
2023-09-05 03:05:55 +0000

    Initial commit

---
+++ cbs.py
@@ -0,0 +1,9 @@
+print("Hello! Welcome to College Budget Simulator! What is your name?")
+
+user_name = str(input())
+
+print("Well" + " " + user_name + " " + "nice to meet you! What college do you attend?")
+
+college_name = str(input())
+
+print("Ahhh I see!" + " " + college_name + " " + "is a great college!")
