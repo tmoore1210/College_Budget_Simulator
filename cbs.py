2f26b4ec197b0b38a70be9f2bf61c93ca821c894
tmoore1210 <93944509+tmoore1210@users.noreply.github.com>
2023-09-12 04:30:53 +0000

    Added more dialogue  and story elements

--- cbs.py
+++ cbs.py
@@ -2,10 +2,50 @@ print("Hello! Welcome to College Budget Simulator! What is your name?")
 
 user_name = str(input())
 
-print("Well" + " " + user_name + " " + "nice to meet you! What college do you attend?")
+print("Nice to meet you" + " " + user_name + "!" + " " +
+      "What college do you attend?")
 
 college_name = str(input())
 
 print("Ahhh I see!" + " " + college_name + " " + "is a great college!")
 
-print("My goal is to help understand whether you are managing your money correctly.")
+print(
+    "My goal is to help understand whether you are managing your money correctly."
+)
+
+print(
+    "How many credits have you completed total? I want to know more about you."
+)
+
+classification = {1: "freshman", 2: "sophomore", 3: "junior", 4: "senior"}
+
+class_info = int(input())
+
+if 0 <= class_info <= 29:
+  print("Oh a" + " " + classification[1] + "!" + " " + "That's great!")
+elif 30 <= class_info <= 59:
+  print("Oh a" + " " + classification[2] + "!" + " " + "That's great!")
+elif 60 <= class_info <= 89:
+  print("Oh a" + " " + classification[3] + "!" + " " + "That's great!")
+elif 90 <= class_info <= 120:
+  print("Oh a" + " " + classification[4] + "!" + " " + "That's great!")
+
+print("Well get ready for some questions!")
+
+print("What do you like to spend money on?")
+
+spend = str(input())
+
+print("Ok. ok. Good things to spend money on!")
+
+print("This may seem private, but how much is in your bank account? This is essential.")
+
+bank_account = int(input("$"))
+
+print("Do you have a job?")
+
+job_info = str(input())
+
+print("What's your form of transportation?")
+
+travel_info = str(input())
